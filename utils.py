030435e11951f9b3df4be415a126a72d73777d82
vadim0702 <54771955+vadim0702@users.noreply.github.com>
2025-04-28 14:46:18 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+import requests
+
+async def get_binance_futures():
+    url = "https://api.binance.com/api/v3/ticker/24hr"
+    response = requests.get(url)
+    return response.json()
+
 def generate_tradingview_link(symbol):
     return f"https://www.tradingview.com/symbols/{symbol}/"
 
