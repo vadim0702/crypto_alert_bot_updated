e93b813e6464a5ff285dba38e8a7f303fca5ec72
vadim0702 <54771955+vadim0702@users.noreply.github.com>
2025-04-28 14:36:51 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
 def generate_tradingview_link(symbol):
     return f"https://www.tradingview.com/symbols/{symbol}/"
+
+def generate_coinglass_link(symbol):
+    return f"https://www.coinglass.com/future/{symbol.replace('USDT', '')}"
